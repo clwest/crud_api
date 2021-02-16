d9b39503f4d84ba8a88a40bb8035501ba41d5296
clwest <westremarketing@gmail.com>
2021-02-16 15:11:10 -0700

    added flask-jwt-extended and tested with postman

--- flask_api_crud/code/app.py
+++ flask_api_crud/code/app.py
@@ -2,9 +2,9 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
-from security import authenticate, identity
-from resources.user import UserRegister, User
+from flask_jwt_extended import JWTManager
+from db import db
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -14,14 +14,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///api_test')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = "chris"
+app.secret_key = "chris" # can also use app.config['JWT_SECRET_KEY']
 api = Api(app)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWTManager(app)
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
@@ -29,8 +29,8 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
