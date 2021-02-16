7402fc58f5d8d5e27ab3529ddbcf53e719297ed3
clwest <westremarketing@gmail.com>
2021-02-16 11:58:28 -0700

    Added PostgreSQL env variable

--- flask_api_crud/code/app.py
+++ flask_api_crud/code/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +11,7 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///api_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///api_test')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "chris"
 api = Api(app)
