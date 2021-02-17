e945acfab6294aa54969be2f8aad9cc8f8cbc7b0
clwest <westremarketing@gmail.com>
2021-02-16 22:37:07 -0700

    loaded jwt_tokens

--- flask_api_crud/code/app.py
+++ flask_api_crud/code/app.py
@@ -3,10 +3,13 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+
+
 from db import db
-from resources.user import UserRegister, User, UserLogin
+from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blocklist import BLOCKLIST
 
 
 
@@ -23,6 +26,55 @@ def create_tables():
 
 jwt = JWTManager(app)
 
+@jwt.additional_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:
+        return {'is_admin': True}
+    return {'is_admin': False}
+
+# @jwt.token_in_blocklist_loader
+# def check_if_token_in_blocklist(jwt_header, jwt_payload, decrypted_token):
+#     return decrypted_token['jti'] in BLOCKLIST
+
+
+# @jwt.expired_token_loader
+# def expired_token_callback(jwt_header, jwt_payload):
+#     return jsonify({
+#         'description': 'The token has expired',
+#         'error': 'token_expired'
+#     }), 401
+
+# @jwt.invalid_token_loader
+# def invalid_token_callback(error):
+#     return jsonify({
+#         'description': 'Signature verification failed.',
+#         'error': 'invalid_token'
+#     }), 401
+
+# @jwt.unauthorized_loader
+# def missing_token_callback(error):
+#     return jsonify({
+#         'description': 'Request does not contain an access code',
+#         'error': 'authorization_required'
+#     }), 401
+
+
+# @jwt.needs_fresh_token_loader # Needs cd jwt_header, jwt_payload
+# def token_not_fresh_callback(jwt_header, jwt_payload):
+#     return jsonify({
+#         'description': 'The token is not fresh',
+#         'error': 'fresh_token_required'
+#     }), 401
+
+# @jwt.revoked_token_loader # Needs cd jwt_header, jwt_payload
+# def revoked_token_callback(jwt_header, jwt_payload):
+#     return jsonify({
+#         'description': 'The token has been revoked',
+#         'error': 'token_revoed'
+#     }), 401
+
+
+
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
@@ -30,6 +82,8 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     db.init_app(app)
--- flask_api_crud/code/resources/user.py
+++ flask_api_crud/code/resources/user.py
@@ -1,7 +1,15 @@
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token, 
+    create_refresh_token, 
+    get_jwt_identity,
+    jwt_required,
+    get_jwt
+)
 from models.user import UserModel
+from blocklist import BLOCKLIST
+
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username', 
@@ -63,4 +71,22 @@ class UserLogin(Resource):
                 'refresh_token': refresh_token
             }, 200
         return {"message": 'Invalid credentials'}, 401
+
+
+# class UserLogout(Resource):
+#     @jwt_required
+#     def post(self):
+#         jti = get_jwt()['jti'] # jti JWT ID 
+#         BLOCKLIST.add(jti)
+#         return {'message': 'Successfully logged out'}, 200
+
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}, 200
+
+
 
