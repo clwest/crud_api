1692f303fe2a8a70bc340485b3fe32ab0bd02006
clwest <westremarketing@gmail.com>
2021-07-15 11:36:30 -0600

    first commit

--- flask_api_crud/code/app.py
+++ flask_api_crud/code/app.py
@@ -32,46 +32,7 @@ def add_claims_to_jwt(identity):
         return {'is_admin': True}
     return {'is_admin': False}
 
-# @jwt.token_in_blocklist_loader
-# def check_if_token_in_blocklist(jwt_header, jwt_payload, decrypted_token):
-#     return decrypted_token['jti'] in BLOCKLIST
 
-
-# @jwt.expired_token_loader
-# def expired_token_callback(jwt_header, jwt_payload):
-#     return jsonify({
-#         'description': 'The token has expired',
-#         'error': 'token_expired'
-#     }), 401
-
-# @jwt.invalid_token_loader
-# def invalid_token_callback(error):
-#     return jsonify({
-#         'description': 'Signature verification failed.',
-#         'error': 'invalid_token'
-#     }), 401
-
-# @jwt.unauthorized_loader
-# def missing_token_callback(error):
-#     return jsonify({
-#         'description': 'Request does not contain an access code',
-#         'error': 'authorization_required'
-#     }), 401
-
-
-# @jwt.needs_fresh_token_loader # Needs cd jwt_header, jwt_payload
-# def token_not_fresh_callback(jwt_header, jwt_payload):
-#     return jsonify({
-#         'description': 'The token is not fresh',
-#         'error': 'fresh_token_required'
-#     }), 401
-
-# @jwt.revoked_token_loader # Needs cd jwt_header, jwt_payload
-# def revoked_token_callback(jwt_header, jwt_payload):
-#     return jsonify({
-#         'description': 'The token has been revoked',
-#         'error': 'token_revoed'
-#     }), 401
 
 
 
